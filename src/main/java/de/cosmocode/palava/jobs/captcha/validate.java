e044b0e90b386e95f286e3bd1b14fd7c71888b72
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-01-21 22:44:32 +0000

    replaced tabs with spaces

---
+++ src/main/java/de/cosmocode/palava/jobs/captcha/validate.java
@@ -0,0 +1,59 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.jobs.captcha;
+
+import java.util.Map;
+
+import com.google.inject.Inject;
+
+import de.cosmocode.palava.Job;
+import de.cosmocode.palava.core.call.Call;
+import de.cosmocode.palava.core.protocol.DataCall;
+import de.cosmocode.palava.core.protocol.Response;
+import de.cosmocode.palava.core.protocol.content.PhpContent;
+import de.cosmocode.palava.core.server.Server;
+import de.cosmocode.palava.core.session.HttpSession;
+import de.cosmocode.palava.services.captcha.Captcha;
+
+/**
+ * Validates the current captcha against the user input.
+ *
+ * @author Willi Schoenborn
+ */
+public class validate implements Job {
+
+    @Inject
+    private Captcha captcha;
+    
+    @Override
+    public void process(Call request, Response response, HttpSession session,
+        Server server, Map<String, Object> caddy) throws Exception {
+        
+        final DataCall req = (DataCall) request;
+        final String userInput = req.getArguments().get("code");
+        final boolean result = captcha.validate(session.getSessionId(), userInput);
+        
+        response.setContent(new PhpContent(result));
+        
+
+
+    }
+
+}
